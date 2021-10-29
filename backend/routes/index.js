37747990859f7e4467cb985e7cdbca5a4b797da1
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-10-29 22:38:54 +0200

    Implement login funcionality

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,11 +4,10 @@ const userController = require("../controllers/user"); // Get all exported funct
 // Map the `signup` request to the signup function
 router.post("/signup", userController.signup);
 
-// router.get("/verify/:confirmationToken", (req, res) => {
-//   res.status(200).json({ msg: "hi" });
-// });
-
 // Map the `verify` request to the verify function
 router.get("/verify/:confirmationToken", userController.verifyEmail);
+
+// Map the `login` request to the login function
+router.post("/login", userController.login);
 
 module.exports = router;
