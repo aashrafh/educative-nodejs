1c7f2a46e3e89815ec50c4e58cde2c5a34d89888
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-12-16 01:00:01 +0200

    Create the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar auth={false} />
+    </>
+  );
 };
 
 export default App;
