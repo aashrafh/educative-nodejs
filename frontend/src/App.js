5708d04791337655c133f9677cbb7e5b307d1d62
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-12-18 03:46:41 +0200

    fix login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useContext, useEffect } from "react";
 import { Context } from "./context";
 import Navbar from "./components/Navbar";
