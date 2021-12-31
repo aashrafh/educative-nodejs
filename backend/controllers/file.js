1c098dbaff22bea711ca462028a84fd417fbdd4c
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-12-31 23:37:24 +0200

    Check spelling for text

--- backend/controllers/file.js
+++ backend/controllers/file.js
@@ -1,6 +1,41 @@
 const { File, validate } = require("../models/file");
+const fs = require("fs");
+const readline = require("readline");
+const SpellChecker = require("spellchecker");
+const stringSimilarity = require("string-similarity");
 
 const BASE_URL = `http://localhost:${process.env.PORT || 5000}/`;
+
+const spellCheck = async (path) => {
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(path),
+    output: process.stdout,
+    console: false,
+  });
+
+  let text = "";
+
+  for await (const line of readInterface) {
+    const correctedLine = line
+      .split(" ")
+      .map((word) => {
+        if (SpellChecker.isMisspelled(word)) {
+          const suggestions = SpellChecker.getCorrectionsForMisspelling(word);
+
+          const matches = stringSimilarity.findBestMatch(word, suggestions);
+
+          return matches.bestMatch.target;
+        } else return word;
+      })
+      .join(" ");
+
+    text += correctedLine + "\n";
+  }
+
+  fs.writeFile(path, text, (err, res) => {
+    if (err) console.log("error", err);
+  });
+};
 
 exports.upload = async (req, res) => {
   try {
@@ -8,6 +43,10 @@ exports.upload = async (req, res) => {
     if (error) return res.status(400).send(error.details[0].message);
 
     const { createdBy, name, description } = req.body;
+
+    if (req.file.mimetype === "text/plain") {
+      await spellCheck(req.file.path);
+    }
 
     const file = await File.create({
       name,
