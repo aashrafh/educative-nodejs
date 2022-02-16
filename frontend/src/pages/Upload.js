612469d89a35a375b7c7f35b7972e4c8f461e684
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-02-16 21:39:52 +0200

    Upload file

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -107,24 +107,7 @@ const Upload = () => {
         <textarea
           onChange={(e) => setDescription(e.target.value)}
           value={description}
-          className="
-          mt-10
-          form-control
-          block
-          w-full
-          px-3
-          py-1.5
-          text-base
-          font-normal
-          text-gray-700
-          bg-white bg-clip-padding
-          border border-solid border-gray-300
-          rounded
-          transition
-          ease-in-out
-          m-0
-          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      "
+          className="mt-10 form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
           rows="3"
           placeholder="File Description"
         ></textarea>
