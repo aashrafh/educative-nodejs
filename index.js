e45a93673407f87362fd7ed77d47761acf968f3d
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2022-03-05 19:02:59 +0200

    Restructure the app

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 require("dotenv").config();
 require("./database/database.js").connect();
 const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
 const router = require("./routes/index");
 const auth = require("./middleware/auth");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("/uploads", express.static("./uploads"));
-// app.use(express.static(path.join(__dirname, "uploads")));
 
-app.get("/", (req, res) => {
-  res.send({ message: "Hello, nodemon!" });
-});
+app.use(express.static(__dirname));
+app.use(favicon(__dirname + "/frontend/build/favicon.ico"));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.post("/hello", auth, (req, res) => {
-  res.status(200).send("Hello 🙌 ");
-});
-
-// Register the application main router
 app.use("/", router);
 
-app.listen(port, () =>
-  console.log(`app is listening at http://0.0.0.0:${port}`)
-);
+app.get("/hello", auth, (req, res) => {
+  res.send("Hello 🙌 ");
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port);
